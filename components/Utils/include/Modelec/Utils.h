b3b6bfdc95d12e1c33c60e91bf0c7e506b61409a
ackimixs <ackimixs@protonmail.com>
2024-05-19 11:04:24 +0200

    mapValue function

--- components/Utils/include/Modelec/Utils.h
+++ components/Utils/include/Modelec/Utils.h
@@ -11,4 +11,9 @@ namespace Modelec
     bool contains(const std::string& str, const std::string& sub);
 
     std::vector<std::string> split(const std::string& str, const std::string& delimiter);
+
+    template<typename T>
+    T mapValue(T v, T v_min, T v_max, T v_min_prime, T v_max_prime) {
+        return v_min_prime + (((v - v_min) * (v_max_prime - v_min_prime)) / (v_max - v_min));
+    }
 }
